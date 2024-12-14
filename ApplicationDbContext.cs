facab3bb93b8eb6f23456e07afc3b8ab1e51fc72
NamrataPDeshmukh <161303191+NamrataPDeshmukh@users.noreply.github.com>
2024-12-14 21:35:11 +0530

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DotNetMachineTest.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("name=DefaultConnection") { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
